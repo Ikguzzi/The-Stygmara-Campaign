506b83659d427c41af7cd0e87cc88185c6a6f867
Ikguzzi <ikguzzi@hotmail.com>
2026-05-25 18:17:59 +0200

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import db from '@astrojs/db';
 
 export default defineConfig({
     output: 'server',
+    adapter: netlify(),
     integrations: [db()],
 });
